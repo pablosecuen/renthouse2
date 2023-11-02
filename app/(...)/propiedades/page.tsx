490e5a9d33212085d78d5802734a2e0d51736a2a
pablosecuen <pablosecuen@gmail.com>
2023-11-02 15:40:46 -0300

    asd

--- app/(...)/propiedades/page.tsx
+++ app/(...)/propiedades/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 function Propiedades() {
   return (
     <div className="min-h-screen pt-24  flex justify-center px-8">
-      <div className="border w-1/2 flex flex-wrap">
+      <div className=" w-1/2 flex flex-wrap">
         <nav className="flex flex-col w-full">
           <h2 className="tracking-widest py-8 text-xl font-semibold ">PROPIEDADES</h2>
           <SearchForm />
@@ -19,7 +19,7 @@ function Propiedades() {
         <Card />
         <Card />
       </div>
-      <aside className="w-1/2 border"></aside>
+      <aside className="w-1/2 flex items-center justify-center"> aca va el mapa</aside>
     </div>
   );
 }
