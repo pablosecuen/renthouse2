540e44ca6c4e4bc4db3606318ea6247d37bd92d9
pablosecuen <pablosecuen@gmail.com>
2023-11-07 20:36:52 -0300

    fix

--- app/firebase.ts
+++ app/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const firestore = getFirestore(app); // Obtenemos la instancia de Firestore
 
 export { firestore }; // Exportamos la instancia de Firestore
