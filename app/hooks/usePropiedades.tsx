fccf91e680a3f8b7ecfbb35ea41aced89d9955c7
pablosecuen <pablosecuen@gmail.com>
2023-11-07 20:26:22 -0300

    custom hook para propiedades database

---
+++ app/hooks/usePropiedades.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import { Propiedad } from "../types/types";
+import { firestore } from "../firebase";
+
+const usePropiedades = (): Propiedad[] => {
+  const [propiedades, setPropiedades] = useState<Propiedad[]>([]);
+
+  useEffect(() => {
+    const propiedadesCollection = collection(firestore, "propiedades");
+    const unsubscribe = onSnapshot(propiedadesCollection, (querySnapshot) => {
+      const propiedadesData: Propiedad[] = [];
+      querySnapshot.forEach((doc) => {
+        const propiedadData = doc.data() as Propiedad;
+        propiedadesData.push(propiedadData);
+      });
+      console.log("Propiedades recuperadas:", propiedadesData);
+      setPropiedades(propiedadesData);
+    });
+
+    return () => {
+      unsubscribe(); // Limpieza cuando el componente se desmonta
+    };
+  }, []); // La dependencia vacía asegura que esto se ejecute solo una vez al montar el componente
+
+  return propiedades;
+};
+
+export default usePropiedades;
