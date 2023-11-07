fde59261e37ca7ebc7f9eb349e3537cdf98cb433
pablosecuen <pablosecuen@gmail.com>
2023-11-07 20:26:28 -0300

    tipados

---
+++ app/types/types.ts
@@ -0,0 +1,44 @@
+export interface Propiedad {
+    id: any;
+    firebaseId: string; 
+    documentoId: string;
+    direccion: string;
+    tipo: string;
+    descripcion: string;
+    huespedes: number;
+    baños: number;
+    ubicacion: string;
+    habitaciones: number;
+    camas: number;
+    precioPorNoche: number;
+    imagenes: string[];
+    distanciadelcentro: number;
+    caracteristicas: {
+      seguridad: boolean;
+      aire: boolean;
+      menores: boolean;
+      mascotas: boolean;
+      fumadores: boolean;
+      balcon: boolean;
+      bañera: boolean;
+      cafetera: boolean;
+      pileta: boolean;
+      parrilla: boolean;
+      wifi: boolean;
+      estacionamiento: boolean;
+    }[];
+    reservas: {
+      start: Date;
+      end: Date;
+      title: string;
+    }[];
+    video: string;
+  }
+
+
+  export interface Reserva {
+    map?: any;
+    start: { seconds: number; nanoseconds: number };
+    end: { seconds: number; nanoseconds: number };
+    title?: string;
+  }
