144dad66735bfbe7e7b5596073b1a3929fa50037
pablosecuen <pablosecuen@gmail.com>
2023-11-07 20:25:15 -0300

    cardcontainer suscripto via custom hook a base de datos

--- app/components/card/card-container.tsx
+++ app/components/card/card-container.tsx
@@ -1,9 +1,13 @@
+"use client";
 import React from "react";
 import Card from ".";
-import { propiedades } from "@/utils";
-import SizeDescription from "./size-description";
-import Link from "next/link";
+
+import usePropiedades from "@/app/hooks/usePropiedades";
+
 function CardContainer({ scale }: any) {
+  const propiedades = usePropiedades();
+
+
   return (
     <div className="flex md:gap-4 z-20 w-full h-full gap-8  md:justify-evenly flex-wrap">
       {propiedades.map((propiedad, index) => (
