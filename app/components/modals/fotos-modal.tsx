264e025534ef0928e2c39cc1de25e89baabd72ab
pablosecuen <pablosecuen@gmail.com>
2023-11-16 16:32:37 -0300

    modal para fotos y fullscreen fotos fix

--- app/components/modals/fotos-modal.tsx
+++ app/components/modals/fotos-modal.tsx
@@ -16,16 +16,16 @@ const FotosModal: React.FC<modalProps> = ({ images, handleModalFotos }) => {
     setFullscreenImage(null);
   };
   return (
-    <div className="h-screen w-screen fixed top-0 left-0 z-50 p-10 bg-white">
+    <div className="min-h-screen w-screen absolute !z-50 top-0 left-0  p-10 bg-white">
       {fullscreenImage ? (
         <>
           <div
-            className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50"
+            className="fixed top-0 left-0 w-full h-full bg-white flex items-center justify-center z-50"
             onClick={handleCloseFullscreen}
           >
             <img src={fullscreenImage} alt="Fullscreen" className="max-h-screen max-w-full" />
           </div>
-          <button onClick={handleModalFotos} className="absolute top-4 right-20">
+          <button onClick={handleModalFotos} className="fixed top-4 right-20 z-50">
             cerrar
           </button>
         </>
