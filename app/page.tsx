7b1ad48e507b1af9bcaf371229f79686aac05970
pablosecuen <pablosecuen@gmail.com>
2024-01-09 13:40:51 -0300

    remove useless script tag

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { withGoogleAnalytics } from "./withGoogleAnalytics";
 const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:px-24 ">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GNNWD1EF17"></script>
+  
       <Landing />
       <Soon />
       <FeedBack />
